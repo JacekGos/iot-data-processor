c2adc6f407feff3524181da990a6e8424d263798
JacekGos <jacekg93@gmail.com>
2025-10-23 14:33:29 +0200

    KAN-5 running mock data sim on app start. Updated config-server configuration in mqtt-to-kafka-service

--- mqtt-to-kafka-service/src/main/java/com/iot/mqtt/to/kafka/service/MqttToKafkaServiceApplication.java
+++ mqtt-to-kafka-service/src/main/java/com/iot/mqtt/to/kafka/service/MqttToKafkaServiceApplication.java
@@ -1,5 +1,8 @@
 package com.iot.mqtt.to.kafka.service;
 
+import com.iot.mqtt.to.kafka.service.runner.StreamRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,12 +12,21 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.iot")
 public class MqttToKafkaServiceApplication implements CommandLineRunner {
+
+    private final Logger LOG = LoggerFactory.getLogger(MqttToKafkaServiceApplication.class);
+    private final StreamRunner streamRunner;
+
+    public MqttToKafkaServiceApplication(StreamRunner streamRunner) {
+        this.streamRunner = streamRunner;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MqttToKafkaServiceApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        LOG.info("running APP");
+        streamRunner.start();
     }
 }
